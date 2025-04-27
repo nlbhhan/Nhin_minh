c57ccede6ada22b460e30246550c0fa151b433b9
nlbhhan <hann.nlbh@gmail.com>
2025-04-27 14:47:59 +0700

    khai bien spacing

--- loop-hinh-tron.js
+++ loop-hinh-tron.js
@@ -1,3 +1,5 @@
+let spacing = 20; //Đặt biến và để spacing bằng 20 luôn
+
 function setup() {
     createCanvas(400, 400);
 }
@@ -5,7 +7,9 @@ function setup() {
     function draw () {
         background(0);
 
-        for(let i = 0; i< 20; i=i+2) {
-            circle(200, 100 + i*20, 50);
+        //Vertically vẽ 10 hình tròn và mỗi hình cách nhau 20 đơn vị (tính từ tâm)
+        for(let i = 0; i< 10; i=i+1) {
+            // circle(200, 100 + i*20, 50); //Nhân 20 thì 20 ở đây là spacing giữa các tâm của hình tròn
+            circle(200, 100 + i*spacing, 50);
         }
     }
