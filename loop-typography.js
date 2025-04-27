a47cc9b1a25e19660f72ac545da934dc8e49476c
nlbhhan <hann.nlbh@gmail.com>
2025-04-27 15:26:59 +0700

    create points to text

--- loop-typography.js
+++ loop-typography.js
@@ -1,7 +1,22 @@
+let font;
+let points; 
+
+function preload() {
+    font = loadFont("assets/1. Font/IBMPlexMono-Medium.ttf");
+ }
+
 function setup() {
     createCanvas(400, 400);
+    points = font.textToPoints("nlbh", 0, height/2, 100, { //trong ngoặc lần lượt là ("text", x, y, size chữ)
+        sampleFactor: 0.1,
+        simplifyThreshold: 0
+    });
 }
 
 function draw() {
-    background(0);
+    background(250);
+    //points là 1 array tập hợp của các điểm tạo nên chữ "nlbh"
+    for (let i=0; i<points.length; i = i+1) { //tại mỗi điểm của chữ thì vẽ 1 cái circle
+        circle(points[i].x, points[i].y, 10); 
+    }
 }
