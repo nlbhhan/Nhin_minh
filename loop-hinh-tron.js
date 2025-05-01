c6697b220e3f6d77d901dc6e6fd601b4673b9f6c
nlbhhan <hann.nlbh@gmail.com>
2025-05-01 09:45:09 +0700

    Remove random

--- loop-hinh-tron.js
+++ loop-hinh-tron.js
@@ -19,7 +19,7 @@ function setup() {
         for (let j=0; j<10; j=j+1) { //Cái này là loop lớn, theo hướng vertical
             for (let i=0; i<10; i=i+1) {//Loop để hình tròn lặp lại theo hướng horizontal
                 let distance = dist(mouseX, mouseY, i*spacing, j*spacing); //Khoảng cách từ mouseX, mouseY đến hình tròn
-                size = random(map(distance, 0, 300, 50, 0)); //Map distance, chuột càng gần thì hình tròn càng lớn, càng xa thì hình tròn càng nhỏ
+                size = map(distance, 0, 300, 50, 0); //Map distance, chuột càng gần thì hình tròn càng lớn, càng xa thì hình tròn càng nhỏ
                 stroke(255);
                 strokeWeight(1);
                 noFill();
