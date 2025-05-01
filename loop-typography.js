1633ae21b0c0afb661da599d4c397e67dbdae912
nlbhhan <hann.nlbh@gmail.com>
2025-05-01 22:08:31 +0700

    new-font

--- loop-typography.js
+++ loop-typography.js
@@ -4,31 +4,32 @@ let sizeX=0;
 let sizeY=0;
 
 function preload() {
-    font = loadFont("assets/1. Font/IBMPlexMono-Medium.ttf");
+    font = loadFont("nhin-minh/assets/1. Font/Unbounded-Bold.ttf");
  }
 
 function setup() {
     createCanvas(500, 500);
-    points = font.textToPoints("languished", 0, 100, 50, { //trong ngoặc lần lượt là ("text", x, y, size chữ)
+    points = font.textToPoints("languished", 0, 100, 60, { //trong ngoặc lần lượt là ("text", x, y, size chữ)
         sampleFactor: 0.3,
         simplifyThreshold: 0
     });
 }
 
 function draw() {
-    background(250);
-    //points là 1 array tập hợp của các điểm tạo nên chữ "nlbh"
+    background(254, 243, 226);
+    //points là 1 array tập hợp của các điểm tạo nên chữ "languished"
 
     
 
     for (let i=0; i<points.length; i = i+1) {
         let distance = dist(mouseX, mouseY, points[i].x, points[i].y);
-        sizeX = map(distance, 0, 200, 10, 0); //Effect cho chữ, càng lia chuột gần thì hình tròn càng to
-    //tại mỗi điểm của chữ thì vẽ 1 cái circle
-        sizeY = map(distance, 0, 200, 0, 10);
+
+        //Effect cho chữ, càng lia gần thì pixels càng bay ra xa
+        sizeX = map(distance, 0, 1000, 10, 0);
+        sizeY = map(distance, 0, 1000, 0, 10);
 
         noStroke()
-        fill(0);
+        fill(243, 198, 35);
         rect(points[i].x, points[i].y, sizeX, sizeY); //Để gọi toạ độ x, y ra thì array[].x và array[].y
     }
 }
