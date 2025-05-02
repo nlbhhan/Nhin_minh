d4aedd29c3aa9c8fd56295db1abbfd1c08dfef0b
nlbhhan <hann.nlbh@gmail.com>
2025-05-02 20:22:41 +0700

    GRA

--- loop-typography.js
+++ loop-typography.js
@@ -2,17 +2,24 @@ let font;
 let pointsLanguished = []; 
 let widthLang=0;
 
+//DIS
 let pointsDis = [];
 let floatDisX=0;
 let floatDisY=0;
 let widthDis;
 let heightDis;
-
-let pointsIntegrated = [];
+//INTE
+let pointsInt = [];
 let floatIntX=0;
 let floatIntY=0;
 let widthInt;
 let heightInt;
+//GRA
+let pointsGra = [];
+let floatGraX = 0;
+let floatGraY = 0;
+let widthGra;
+let heightGra;
 
 function preload() {
     font = loadFont("assets/1-Font/Unbounded-Bold.ttf");
@@ -20,17 +27,23 @@ function preload() {
 
 function setup() {
     createCanvas(500, 500);
-    pointsLanguished = font.textToPoints("languished", -170, 0, 60, { //trong ngoặc lần lượt là ("text", x, y, size chữ)
-        sampleFactor: 0.7,
-        simplifyThreshold: 0.0001
-    });
+    // pointsLanguished = font.textToPoints("languished", -170, 0, 60, { //trong ngoặc lần lượt là ("text", x, y, size chữ)
+    //     sampleFactor: 0.7,
+    //     simplifyThreshold: 0.0001
+    // });
 
-    pointsDis = font.textToPoints("DIS", -230, -50, 150, {
+    //DIS
+    pointsDis = font.textToPoints("DIS", -200, -50, 120, {
         sampleFactor: 0.7,
         simplyfyThreshold: 0
     });
-
-    pointsIntegrated = font.textToPoints("INTEGRATED", -220, 80, 80, {
+    //INTE
+    pointsInt = font.textToPoints("INTE", -200, 50, 120, {
+        sampleFactor: 0.7,
+        simplifyThreshold: 0
+    });
+    //GRA
+    pointsGra = font.textToPoints("GRA", -200, 150, 120, {
         sampleFactor: 0.7,
         simplifyThreshold: 0
     });
@@ -78,10 +91,10 @@ function draw() {
     }
     pop();
     
-    //Integrated, giây càng tăng thì cục pixels fade dần
+    //Inte giây càng tăng thì cục pixels fade dần
     push();
     translate(200, 200);
-    for (let k=0; k<pointsIntegrated.length; k=k+1) {
+    for (let k=0; k<pointsInt.length; k=k+1) {
 
         //Hiệu ứng lơ lửng cho hình chữ nhật
         floatIntX = map(noise(k * 0.5, frameCount * 0.05), 0, 1, -2, 2); //Lơ lửng theo trục X
@@ -95,9 +108,30 @@ function draw() {
         // strokeWeight(1);
         noStroke();
         fill(255, 178, 44);
-        rect(pointsIntegrated[k].x + floatIntX, pointsIntegrated[k].y + floatIntY, widthInt, heightInt);
+        rect(pointsInt[k].x + floatIntX, pointsInt[k].y + floatIntY, widthInt, heightInt);
     }
     pop();
 
+
+    //Gra
+    //Inte giây càng tăng thì cục pixels fade dần
+    push();
+    translate(200, 200);
+    for (let l=0; l<pointsGra.length; l=l+1) {
+
+        //Hiệu ứng lơ lửng cho hình chữ nhật
+        floatGraX = map(noise(l * 0.5, frameCount * 0.05), 0, 1, -2, 2); //Lơ lửng theo trục X
+        floatGraY = map(noise(l * 0.5 + 100, frameCount * 0.05), 0, 1, -2, 2); //Lơ lửng theo trục Y
+        
+        //Map size của hcm theo second(), giây càng tăng thì size càng bé
+        widthGra = map(second(), 0, 60, 30, 10);
+        heightGra = map(second(), 0, 60, 10, 0);
+        
+        // noFill();
+        // strokeWeight(1);
+        noStroke();
+        fill(255, 178, 44);
+        rect(pointsGra[l].x + floatGraX, pointsGra[l].y + floatGraY, widthGra, heightGra);
+    }
 
 }
