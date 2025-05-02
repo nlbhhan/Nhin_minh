1826f6e78d3a0ee48d265c5fe0025b83cb6f1485
nlbhhan <hann.nlbh@gmail.com>
2025-05-02 19:08:07 +0700

    draw a flower

--- flower.js
+++ flower.js
@@ -1,7 +1,16 @@
 function setup() {
     createCanvas(400, 400);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(250);
+    push();
+    translate(200, 200);
+
+    for (let i=0; i<360; i=i+30) {
+        rotate(30);
+    ellipse(0, -50, 20, 70);
+    circle(0, 0, 50);
+    }
 }
