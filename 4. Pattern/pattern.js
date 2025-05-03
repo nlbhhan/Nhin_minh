232e2887e24f9e4b2ffeb7a6187030f084d1a05f
nlbhhan <hann.nlbh@gmail.com>
2025-05-03 20:52:28 +0700

    add font

---
+++ 4. Pattern/pattern.js
@@ -0,0 +1,13 @@
+let font;
+
+function preload() {
+    font = loadFont("assets/1-Font/DanhDa-Bold.otf");
+ }
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background("magenta");
+}
