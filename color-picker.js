d70eb568915793ad447c09a9bb61bc1af2704a05
nlbhhan <hann.nlbh@gmail.com>
2025-05-03 15:00:00 +0700

    updates note

--- color-picker.js
+++ color-picker.js
@@ -5,8 +5,19 @@ function preload() {
 
 function setup() {
     createCanvas(400, 400);
+    img.resize(0, height);
 }
 
 function draw() {
-    background(255);
+    background(0);
+
+    //Hiển thị hình
+    imageMode(CORNER);
+    image(img, 0, 0);
+
+    //Color picker
+    let c = img.get(mouseX, mouseY); //.get là lấy màu, lấy màu ngay tại vị trí con chuột
+    // console.log(c);
+    fill(c);
+    rect(300, 0, 100, height);
 }
