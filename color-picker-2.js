56a0d205bc9e061fa8a321e3e527c5bab74c11f4
nlbhhan <hann.nlbh@gmail.com>
2025-05-03 15:05:06 +0700

    pick hinh

---
+++ color-picker-2.js
@@ -0,0 +1,23 @@
+let img;
+function preload() {
+    img = loadImage("assets/2.jpg");
+}
+
+function setup() {
+    createCanvas(400, 400);
+    img.resize(0, height);
+}
+
+function draw() {
+    background(0);
+
+    //Hiển thị hình
+    imageMode(CORNER);
+    image(img, 0, 0);
+
+    //Color picker
+    let c = img.get(mouseX-20, mouseY-20, mouseX+20, mouseY+20); //.get là lấy màu, lấy màu ngay tại vị trí con chuột
+    // console.log(c);
+    fill(c);
+    rect(300, 0, 100, height);
+}
