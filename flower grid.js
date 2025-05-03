fd2e550c7753f78e4bdc1a3dd35b941ebeb53eb5
nlbhhan <hann.nlbh@gmail.com>
2025-05-03 13:23:59 +0700

    horizontal

--- flower grid.js
+++ flower grid.js
@@ -1,3 +1,5 @@
+let spacing = 10;
+
 function setup() {
     createCanvas(400, 400);
     angleMode(DEGREES);
@@ -6,8 +8,12 @@ function setup() {
 function draw() {
     background(250);
     translate(200, 200);
-    for(let i=0; i<360; i=i+30) {
-        rotate(30);
-        ellipse(0, 0, 20, 70);
+    for(let j=-5; j<20; j=j+5) {
+        for(let i=0; i<360; i=i+30) {
+            rotate(30);
+            ellipse(0, 50, 20, 70);
+        }
+        circle(j*spacing, 0, 40);
     }
+    
 }
