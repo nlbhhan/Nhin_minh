a32d6b6fe4a2a26eac7e45e92b58ac8f8cbcf477
nlbhhan <hann.nlbh@gmail.com>
2025-05-04 14:45:16 +0700

    update notes

--- 4. Color-picker/color-picker-2.js
+++ 4. Color-picker/color-picker-2.js
@@ -16,7 +16,7 @@ function draw() {
     image(img, 0, 0);
 
     //Color picker
-    let c = img.get(mouseX-20, mouseY-20, 100, 100); //cắt hình
+    let c = img.get(mouseX-20, mouseY-20, 100, 100); //cắt hình, get 1 cái vùng 20-20 xung quanh con chuột để hiện thị lên
     imageMode(CORNER);
     image(c, 300, 0, 100, 100); //display hình c tại vị trí 300, 0 và hình díplay có size là W 100 và H 100
 
