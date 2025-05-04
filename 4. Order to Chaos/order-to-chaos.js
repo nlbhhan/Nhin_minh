68d91dd62a5d29362902d724a4f8bf4b0d219037
nlbhhan <hann.nlbh@gmail.com>
2025-05-04 09:39:29 +0700

    edit

--- 4. Order to Chaos/order-to-chaos.js
+++ 4. Order to Chaos/order-to-chaos.js
@@ -1,15 +1,24 @@
 let spacing = 25;
 
+
 function setup() {
     createCanvas(400, 400);
+    // rectMode(CENTER);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(240);
-    for(let j=0; j<height/spacing; j=j+1) {
-        for(let i=0; i<width/spacing; i=i+1) {
-            square(0 + i*spacing, 0 + j*spacing, 50);
+    let angle = 0;
+    for(let j=0; j<height; j=j+20) {
+        for(let i=0; i<width; i=i+20) {
+            angle = angle+0.1; //Mỗi lần lặp lại thì angle sẽ công thêm 0.1
+
+            push();
+            translate(j, i); //Tại mỗi toạ độ (j,i) thì vẽ một hình vuông ngay tâm
+            rotate(angle); //Mỗi lần lặp lại thì hình vuông sẽ rotate 1 góc = angle 
+            square(0, 0, 50);
+            pop();
         }
-    }
-    
+        }
 }
