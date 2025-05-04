402a231246ad5c285b4d2deec8cbb39de2b1f8b9
nlbhhan <hann.nlbh@gmail.com>
2025-05-04 15:07:03 +0700

    map pixel

--- 7. ascii/ascii.js
+++ 7. ascii/ascii.js
@@ -1,4 +1,5 @@
 let img;
+let chuoiKiTu = "     .:░▒▓█"; //String là một tập hợp các kí tự, tương tự như Array
 
 function preload() {
     img = loadImage("3.jpg");
@@ -11,18 +12,39 @@ function setup() {
 }
 
 function draw() {
-    // background(0);
-    
-    img.loadPixels();
-    //Invert color
-    for (let i=0; i<img.pixels.length; i=i+4) { //1 tập hợp pixels bao gồm 4 dữ liệu R, G, B và A(Alpha) nên i phải nhảy 4 bước để đi tới tập hợp tiếp theo
-        img.pixels[i] = 255 - img.pixels; //Lớp đầu tiên - Red
-        img.pixels[i+1] = 255 - img.pixels[i+1]; //Lớp thứ 2 - Green
-        img.pixels[i+2] = 255 - img.pixels[i+2]; // Lớp thứ 3 - Blue
-        //Không có lớp thứ 4 vì không invert được lớp Alpha
+    background(240);
+    image(img, 0, 0);
+    img.loadPixels(); 
+
+        //Invert color
+        // for (let i=0; i<img.pixels.length; i=i+4) { //1 tập hợp pixels bao gồm 4 dữ liệu R, G, B và A(Alpha) nên i phải nhảy 4 bước để đi tới tập hợp tiếp theo
+        //     img.pixels[i] = 255 - img.pixels; //Lớp đầu tiên - Red
+        //     img.pixels[i+1] = 255 - img.pixels[i+1]; //Lớp thứ 2 - Green
+        //     img.pixels[i+2] = 255 - img.pixels[i+2]; // Lớp thứ 3 - Blue
+            //Không có lớp thứ 4 vì không invert được lớp Alpha
+
+    for (let i=0; i<img.height; i=i+1) {
+        for (let j=0; j < img.width; j = j+1) {
+            idx=(i*width+j)*4;
+            //Tính độ sáng trung bình của hình, coi pixel ngay chỗ đó nó sáng bao nhiêu 
+            let r = img.pixels[idx]; //Red
+            let g = img.pixels[idx+1]; //Green
+            let b = img.pixels[idx+2]; //Blue
+            let avg = (r+g+b)/3;
+
+            //Hiện thị index theo độ sáng của hình
+            //Bỏ dô int() để nó hiện thị số nguyên
+            let charIndex = int(map(avg, 0, 255, 0, chuoiKiTu.length)); //Độ sáng chạy từ 0 đến 255, pixels chạy từ 0 đến length của chuoiKiTu
+            let char = chuoiKiTu[charIndex]; //Có index rồi thì hiển thị nó ra
+            textSize(5);
+            fill(0);
+            noStroke();
+            text(char, j*5, i*5);   
+        }
     }
+
 
     img.updatePixels(); //Nhớ phải có bước này để hình hiển thị các thay đổi mình đã làm ở trên
     // console.log(img.pixels);
-    image(img, 0, 0);
+    
 }
