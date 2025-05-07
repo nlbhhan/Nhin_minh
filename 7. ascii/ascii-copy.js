5256836dcbbc6c5973a5e60cda439b0f4df484b1
nlbhhan <hann.nlbh@gmail.com>
2025-05-07 21:35:00 +0700

    experimental

--- 7. ascii/ascii-copy.js
+++ 7. ascii/ascii-copy.js
@@ -1,32 +1,31 @@
 let img;
 let font;
 let semiBold;
+let soraMedium;
+let soraExtraBold;
 // let chuoiKiTu = "     .:░▒▓█"; 
-let chuoiKiTu= "DDDD_______       OOOOOOO       ____________";
+let chuoiKiTu= "__________________###############";
+let chuoiKiTu2 = "                _______________";
 
 function preload() {
-    img = loadImage("Imgs/5.jpg");
+    img = loadImage("Imgs/7.jpg");
     font = loadFont("font/Spectral-ExtraLight.ttf");
     semiBold = loadFont("font/Spectral-SemiBoldItalic.ttf");
+    soraMedium = loadFont("font/Sora-Medium.ttf");
+    soraExtraBold = loadFont("font/Sora-ExtraBold.ttf");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth,  windowHeight);
     img.resize(500, 0);
     noLoop();
 }
 
 function draw() {
-    background(248, 248, 255);
+    // background(248, 248, 255);
+    background(0);
     // image(img, 0, 0);
     img.loadPixels(); 
-
-        //Invert color
-        // for (let i=0; i<img.pixels.length; i=i+4) { //1 tập hợp pixels bao gồm 4 dữ liệu R, G, B và A(Alpha) nên i phải nhảy 4 bước để đi tới tập hợp tiếp theo
-        //     img.pixels[i] = 255 - img.pixels; //Lớp đầu tiên - Red
-        //     img.pixels[i+1] = 255 - img.pixels[i+1]; //Lớp thứ 2 - Green
-        //     img.pixels[i+2] = 255 - img.pixels[i+2]; // Lớp thứ 3 - Blue
-            //Không có lớp thứ 4 vì không invert được lớp Alpha
 
     for (let i=0; i<img.height; i=i+1) {
         for (let j=0; j < img.width; j = j+1) {
@@ -43,6 +42,19 @@ function draw() {
             let charIndex = int(map(avg, 0, 255, 0, chuoiKiTu.length)); //Độ sáng chạy từ 0 đến 255, pixels chạy từ 0 đến length của chuoiKiTu
             let char = chuoiKiTu[charIndex]; //Có index rồi thì hiển thị nó ra
 
+            let charIndex2 = int(map(avg, 255, 0, chuoiKiTu2.length));
+            let char2 = chuoiKiTu2[charIndex2];
+
+            //Third layer
+            textSize(12);
+            textLeading(60);
+            textAlign(TOP, LEFT);
+            textFont(soraExtraBold);
+            // fill(78, 215, 241); 
+            fill(255, 240);
+            noStroke();
+            text(charIndex2, width-j*20, i*10); 
+
             //First layer
             textSize(24);
             textLeading(45);
@@ -50,20 +62,32 @@ function draw() {
             textFont(font);
             fill(255, 144, 187); 
             noStroke();
-            text(char, j*10, i*10);  
+            text(char, j*10, i*15);  
 
             //Second layer
-            textSize(20);
-            textLeading(45);
+            textSize(12);
+            textLeading(60);
             textAlign(CENTER);
-            textFont(semiBold);
+            textFont(soraExtraBold);
             fill(78, 215, 241); 
             noStroke();
-            text(char, j*15, i*15);  
+            text(char, width - j*4, i*20);  
 
-
+            
         }
     }
+
+        //Quote
+        textSize(36);
+        textAlign(TOP, LEFT);
+        textFont(soraMedium);
+        textLeading(36);
+        fill("white");
+        noStroke();
+        text('To me, there is no better way', 200, 200);
+        text('to release my mind', 400, 236);
+        text('than to go do', 600, 272);
+        text('Photography and Videography.', 800, 308);
 
 
     img.updatePixels(); //Nhớ phải có bước này để hình hiển thị các thay đổi mình đã làm ở trên
