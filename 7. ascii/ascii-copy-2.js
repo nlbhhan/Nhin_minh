6594c4106bb1c5a564d6928d3e52e0ccd520e731
nlbhhan <hann.nlbh@gmail.com>
2025-05-10 14:53:09 +0700

    ascii-slider

--- 7. ascii/ascii-copy-2.js
+++ 7. ascii/ascii-copy-2.js
@@ -4,8 +4,10 @@ let semiBold;
 let soraMedium;
 let soraExtraBold;
 // let chuoiKiTu = "     .:░▒▓█"; 
-let chuoiKiTu= "__________________###############";
-let chuoiKiTu2 = "     .:░▒▓█";
+let chuoiKiTu= "__________________@@@@@@@@@@@@@@@@@@###############";
+
+//Slider -Thiết lập thanh kéo
+let slider;
 
 function preload() {
     img = loadImage("Imgs/8.jpg");
@@ -17,9 +19,10 @@ function preload() {
 
 function setup() {
     createCanvas(1080,  windowHeight);
-    // img.resize(300, 0);
     angleMode(DEGREES);
-    noLoop();
+    slider = createSlider(10, 200, 10, 1); //min Text size, max text size, default là 10, và mỗi lần kéo thì cộng/trừ 1 đơn vị
+    slider.position(1, height/2);
+    // noLoop();
 }
 
 function draw() {
@@ -42,75 +45,19 @@ function draw() {
             //Bỏ dô int() để nó hiện thị số nguyên
             let charIndex = int(map(avg, 0, 255, 0, chuoiKiTu.length)); //Độ sáng chạy từ 0 đến 255, pixels chạy từ 0 đến length của chuoiKiTu
             let char = chuoiKiTu[charIndex]; //Có index rồi thì hiển thị nó ra
-
-            let charIndex2 = int(map(avg, 255, 0, chuoiKiTu2.length));
-            let char2 = chuoiKiTu2[charIndex2];
             
-            // //First layer
-            // push();
-            // translate(200, 0);
-            // textSize(12);
-            // textLeading(60);
-            // textAlign(CENTER);
-            // textFont(soraExtraBold);
-            // fill(255); 
-            // noStroke();
-            // rotate(90);
-            // text(char, j*10, i*5);  
-            // pop();
-            
-            //Second layer
-            // textSize(24);
-            // textLeading(45);
-            // textAlign(CENTER);
-            // textFont(semiBold);
-            // fill(5, 191, 219); 
-            // noStroke();
-            // text(char2, width-j*4, i*4); 
-
             //Third layer
-            //Feedback: chỉnh lại hình xuống 20-35, chỉnh text Size lại nhỏ xún
-            textSize(12);
+            textSize(slider.value());
             textLeading(45);
             textAlign(CENTER);
             textFont(font);
-            fill(r+100, g, b+50); 
+            fill(r+50, g-30, b+20); 
             noStroke();
-            text(char, j*4, i*4);
-
-            
-
-            
-
-            // //First layer
-            // textSize(24);
-            // textLeading(45);
-            // textAlign(CENTER);
-            // textFont(font);
-            // fill(255, 144, 187); 
-            // noStroke();
-            // text(char, width-j*10, i*5);  
-
+            text(char, j*10, i*10);
             
             
         }
     }
-
-        // //Quote
-        // textSize(36);
-        // textAlign(TOP, LEFT);
-        // textFont(soraMedium);
-        // textLeading(36);
-        // fill("white");
-        // noStroke();
-        // text('To me, there is no better way', 100, 500);
-        // text('to release my mind', 400, 536);
-        // text('than to go do', 250, 572);
-        // // textAlign(TOP, RIGHT);
-        // text('Photography', 250, 608);
-    
-
-
     img.updatePixels(); //Nhớ phải có bước này để hình hiển thị các thay đổi mình đã làm ở trên
     // console.log(img.pixels);
     
