19143dab33f22e65c4f6f039a033ab25e44a12d1
nlbhhan <hann.nlbh@gmail.com>
2025-05-11 11:24:41 +0700

    adding-underlayer

--- 7. ascii/ascii-practice.js
+++ 7. ascii/ascii-practice.js
@@ -8,7 +8,7 @@ let symbol = "     .:░▒▓█";
 let slider;
 
 function preload() {
-    font = loadFont ("font/Sora-Medium.ttf");
+    font = loadFont ("font/Tiny5-Regular.ttf");
     img = loadImage("Imgs/9.jpg");
 }
 
@@ -41,23 +41,43 @@ function draw() {
             let charIndex = int(map(grayScale, 0, 255, 0, chuoiKiTu.length));
             let char = chuoiKiTu[charIndex];
 
-            let idxSymbol = int(map(grayScale, 0, 255, 0, symbol.length));
+            let idxSymbol = int(map(grayScale, 50, 255, 0, symbol.length));
             let chuoiSymbol = symbol[idxSymbol];
 
+            //Underlayer
+            textSize(5);
+            fill(r+100,g,b+50, 190);
+            noStroke();
+            text(char, j*8, i*8);
+
+
             textSize(slider.value());
-            // Nếu giây chẵn thì đổi hình sang symbol, nếu giây lẻ thì đổi hình sang chuoiKyTu
-            if (second()%2 ==0) {
-                textLeading(14);
-                fill(r-20,g,b);
-                noStroke();
-                text(char, j*7, i*7);
-            } else {
+            // // Nếu giây chẵn thì đổi hình sang symbol, nếu giây lẻ thì đổi hình sang chuoiKyTu
+            // if (second()%2 ==0) {
+            //     textLeading(14);
+            //     fill(r-10,g-255,b-20);
+            //     noStroke();
+            //     // text(char, j*7, i*7);
+            //     ellipse(j*7, i*7, 5, 50);
+            // } else {
+
                 fill(r+100,g,b+50);
                 noStroke();
                 text(chuoiSymbol, j*7, i*7);
-            }
             
         }
     }
     img.updatePixels();
+
+     //Text
+    push();
+    textSize(180);
+    textLeading(110);
+    textFont(font);
+    // fill(155, 126, 189);
+    fill(255);
+    textAlign(TOP, LEFT);
+    text("The\nOpera\nHouse", 10, 120);
+    pop();           
+
 }
